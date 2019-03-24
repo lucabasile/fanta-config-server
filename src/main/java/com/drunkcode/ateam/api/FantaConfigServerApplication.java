f92776d8877cffec8b9188c0e3c394facad24ca9
Basile Luca <BasileLu@valueteam.com>
2019-03-24 18:51:19 +0100

    First commit

---
+++ src/main/java/com/drunkcode/ateam/api/FantaConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.drunkcode.ateam.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class FantaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drunkcode/ateam/api/FantaConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drunkcode.ateam.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantaConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
